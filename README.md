0c3a54e1c3ea75be6ada9234a4bc683ea4bcc168
Thomas Brüggemann <mail@thomasbrueggemann.com>
2023-12-29 00:31:29 +0100

    done

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# midi-preview
-🎹 Preview MIDI files through a VST plugin
+# MIDI 🎹 Preview
+
+Preview MIDI files through a VST/VST3/AU plugin.
+It generates 5 sec. wav files of rendered MIDI through an instrument plugin of your choice
+
+```bash
+usage: midi-preview.py [-h] [-m MIDI] [-p PLUGIN]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m MIDI, --midi MIDI  Path to your midi files
+  -p PLUGIN, --plugin PLUGIN
+                        Path to VST/VST3/AU plugin
+```
---
+++ midi-preview.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import dawdreamer as daw
+from scipy.io import wavfile
+from playsound import playsound
+import os
+from tqdm import tqdm
+
+SAMPLE_RATE = 44100
+BUFFER_SIZE = 128
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-m", "--midi", help="Path to your midi files")
+argParser.add_argument("-p", "--plugin", help="Path to VST/VST3/AU plugin")
+
+args = argParser.parse_args()
+
+engine = daw.RenderEngine(SAMPLE_RATE, BUFFER_SIZE)
+plugin = engine.make_plugin_processor("midi-preview", args.plugin)
+engine.load_graph([(plugin, [])])
+
+midi_files = []
+
+print("Searching for midi files in " + args.midi)
+for root, dirs, files in os.walk(args.midi):
+	for file in files:
+		if file.endswith(".mid"):
+			file_path = os.path.join(root, file)
+			midi_files.append(file_path)
+
+print("Found " + str(len(midi_files)) + " midi files")
+print("Start rendering...")
+
+bar = tqdm(midi_files)
+for midi_file in bar:
+	plugin.load_midi(midi_file, clear_previous=True, beats=False, all_events=True)
+
+	engine.render(5) 
+	audio = engine.get_audio()
+
+	filename = os.path.splitext(midi_file)[0] + ".wav"
+	bar.set_postfix_str(filename)
+	wavfile.write(filename, SAMPLE_RATE, audio.transpose())
